d10778e1859a2a740864b6b26a37ff35777e6a50
unknown <brett_matsumoto@yahoo.com>
2019-02-19 04:50:27 -1000

    Completed question 12

--- exercises.js
+++ exercises.js
@@ -121,6 +121,14 @@ console.log('Question 11(b): "three" === "3" is ' + res2 + '.');
 
  console log the object*/
 
+ var hotel = {
+    name: 'Hilton',
+    rooms: 200,
+    pool: true,
+    checkIn: null,
+ }
+ console.log(hotel);
+
 /*13.  Declare a literal object named "car" with properties and values set to:
 
     make  => "BMW"
